a0313de54fab42b6bdf69e952ce137b6552fb9c5
Bryce Wilson <bryce@thenetworknerds.ca>
2019-04-12 20:40:52 -0700

    Added prompt for Day11

--- Day11.py
+++ Day11.py
@@ -1,4 +1,19 @@
+"""
+Prompt:
+There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function
+that returns the number of unique ways you can climb the staircase. The order of the steps matters.
 
+For example, if N is 4, then there are 5 unique ways:
+
+1, 1, 1, 1
+2, 1, 1
+1, 2, 1
+1, 1, 2
+2, 2
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive
+integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+"""
+__author__ = "Bryce Wilson"
 #  Copyright (c) Bryce Wilson 2019.
 #  This program is probably useless so no warranty is given, even the implied warranty of being
 #  suitable for any specific use as no specific use is suggested.
