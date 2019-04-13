6cbedcc2a22ea9a1627b6fac16e7f0d88039b4ca
Bryce Wilson <bryce@thenetworknerds.ca>
2019-04-12 20:36:47 -0700

    Added gitignore and stub of day 11

---
+++ Day11.py
@@ -0,0 +1,7 @@
+
+#  Copyright (c) Bryce Wilson 2019.
+#  This program is probably useless so no warranty is given, even the implied warranty of being
+#  suitable for any specific use as no specific use is suggested.
+#  This program is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
+#   4.0 International (CC BY-NC-SA 4.0) https://creativecommons.org/licenses/by-nc-sa/4.0/
+
