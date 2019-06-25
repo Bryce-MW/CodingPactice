025a5d8e910f3f33792bb0e794f863b949ef4cad
Bryce Wilson <bryce@thenetworknerds.ca>
2019-06-24 22:48:31 -0700

    Got through a few more days (19 and 20)

--- Day19.py
+++ Day19.py
@@ -9,9 +9,54 @@ In this example, assume nodes with the same value are the exact same node object
 Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
 """
 __author__ = "Bryce Wilson"
+
+
 #  Copyright (c) Bryce Wilson 2019.
 #  This program is probably useless so no warranty is given, even the implied warranty of being
 #  suitable for any specific use as no specific use is suggested.
 #  This program is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
 #  4.0 International (CC BY-NC-SA 4.0) https://creativecommons.org/licenses/by-nc-sa/4.0/
 
+class Element:
+    """
+    An element of a single linked list. Has one reference to the next element
+    """
+
+    def __init__(self, next_element):
+        self.next_element: Element = next_element
+
+
+def find_overlap(list1: Element, list2: Element) -> Element:
+    """
+    Finds the element where both lists become one
+    :param list1: the first list
+    :param list2: the second list
+    :return: the element where the lists combine
+    """
+    special_element = Element(None)
+
+    current_element = list2
+    while current_element.next_element is not None:
+        next_element = current_element.next_element
+        current_element.next_element = special_element
+        current_element = next_element
+
+    current_element = list1
+    while current_element.next_element is not special_element:
+        current_element = current_element.next_element
+    return current_element
+
+
+if __name__ == "__main__":
+    first = Element(None)
+    second = first
+
+    for i in range(5):
+        first = Element(first)
+        if i == 3:
+            second = first
+
+    for i in range(5):
+        second = Element(first)
+
+    print(f"{find_overlap(first, second) = }")
--- Day20.py
+++ Day20.py
@@ -5,6 +5,9 @@ rooms required.
 
 For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 """
+from typing import Tuple, List
+from collections import defaultdict
+
 __author__ = "Bryce Wilson"
 #  Copyright (c) Bryce Wilson 2019.
 #  This program is probably useless so no warranty is given, even the implied warranty of being
@@ -12,3 +15,19 @@ __author__ = "Bryce Wilson"
 #  This program is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
 #  4.0 International (CC BY-NC-SA 4.0) https://creativecommons.org/licenses/by-nc-sa/4.0/
 
+
+def min_classrooms(timetable: List[Tuple[int, int]]) -> int:
+    """
+    Returns the minimum number of classrooms that are needed for the timetable of start and end points
+    :param timetable:
+    :return:
+    """
+    rooms = defaultdict(lambda: 0)
+    for i in timetable:
+        for j in range(i[0], i[1]):
+            rooms[j] += 1
+    return max(rooms.values())
+
+
+if __name__ == "__main__":
+    print(f"{min_classrooms([(30, 75), (0, 50), (60, 150)]) = }")
