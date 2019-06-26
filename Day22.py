689b58fbdce326237217b296f64b7db3a0b25659
Bryce Wilson <bryce@thenetworknerds.ca>
2019-06-26 15:45:56 -0700

    Completed day 22

--- Day22.py
+++ Day22.py
@@ -10,6 +10,8 @@ For example, given the set of words 'quick', 'brown', 'the', 'fox', and the stri
 Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either
 ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
 """
+from collections import defaultdict
+
 __author__ = "Bryce Wilson"
 #  Copyright (c) Bryce Wilson 2019.
 #  This program is probably useless so no warranty is given, even the implied warranty of being
@@ -17,3 +19,27 @@ __author__ = "Bryce Wilson"
 #  This program is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
 #  4.0 International (CC BY-NC-SA 4.0) https://creativecommons.org/licenses/by-nc-sa/4.0/
 
+from typing import List
+
+
+def originate(combined: str, dictionary: List[str]) -> List[str]:
+    """
+    Finds the list of the original sentence for one with the removed spaces
+    :param combined:
+    :param dictionary:
+    :return:
+    """
+    if combined == "":
+        return [" "]
+    starters: List[str] = [i for i in dictionary if combined.startswith(i)]
+    if not starters:
+        return []
+    options: List[List[str]] = [[i] + j for i in starters if (j:= originate(combined[len(i):], dictionary))]
+    if not options:
+        return []
+    return [i for i in options[0] if i.rstrip()]
+
+
+if __name__ == "__main__":
+    print(f"{originate('thequickbrownfox', ['quick', 'brown', 'the', 'fox']) = }")
+    print(f"{originate('bedbathandbeyond', ['bed', 'bath', 'bedbath', 'and', 'beyond']) = }")
