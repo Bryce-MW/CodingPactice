a1a4b96db275be06cdd4f4d676180c450530c652
Bryce Wilson <bryce@thenetworknerds.ca>
2019-06-26 20:11:44 -0700

    Completed day 23

--- Day23.py
+++ Day23.py
@@ -23,3 +23,72 @@ __author__ = "Bryce Wilson"
 #  This program is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
 #  4.0 International (CC BY-NC-SA 4.0) https://creativecommons.org/licenses/by-nc-sa/4.0/
 
+from typing import List, Tuple
+
+
+def path(wall_map: List[List[bool]], pathed: List[List[bool]], loc: Tuple[int, int], final: Tuple[int, int]) -> int:
+    if loc[0] + 1 == final[0] and loc[1] == final[1] or \
+            loc[0] - 1 == final[0] and loc[1] == final[1] or \
+            loc[1] + 1 == final[1] and loc[0] == final[0] or \
+            loc[1] - 1 == final[1] and loc[0] == final[0] :
+        return 1
+    next_pathed = [[j for j in i] for i in pathed]
+    try:
+        next_pathed[loc[0] + 1][loc[1]] = True
+    except IndexError:
+        pass
+    try:
+        next_pathed[loc[0] - 1][loc[1]] = True
+    except IndexError:
+        pass
+    try:
+        next_pathed[loc[0]][loc[1] + 1] = True
+    except IndexError:
+        pass
+    try:
+        next_pathed[loc[0]][loc[1] - 1] = True
+    except IndexError:
+        pass
+    options = []
+    try:
+        if not (wall_map[loc[0] + 1][loc[1]] or pathed[loc[0] + 1][loc[1]]):
+            next_length = path(wall_map, next_pathed, (loc[0] + 1, loc[1]), final)
+            if next_length:
+                options.append(next_length + 1)
+    except IndexError:
+        pass
+    try:
+        if not (wall_map[loc[0] - 1][loc[1]] or pathed[loc[0] - 1][loc[1]]):
+            next_length = path(wall_map, next_pathed, (loc[0] - 1, loc[1]), final)
+            if next_length:
+                options.append(next_length + 1)
+    except IndexError:
+        pass
+    try:
+        if not (wall_map[loc[0]][loc[1] + 1] or pathed[loc[0]][loc[1] + 1]):
+            next_length = path(wall_map, next_pathed, (loc[0], loc[1] + 1), final)
+            if next_length:
+                options.append(next_length + 1)
+    except IndexError:
+        pass
+    try:
+        if not (wall_map[loc[0]][loc[1] - 1] or pathed[loc[0]][loc[1] - 1]):
+            next_length = path(wall_map, next_pathed, (loc[0], loc[1] - 1), final)
+            if next_length:
+                options.append(next_length + 1)
+    except IndexError:
+        pass
+    if not options:
+        return 0
+    return min(options)
+
+
+if __name__ == "__main__":
+    print(f"""{path([[False, False, False, False],
+[True, True, False, True],
+[False, False, False, False],
+[False, False, False, False]],
+[[False, False, False, False],
+[False, False, False, False],
+[False, False, False, False],
+[False, False, False, False]], (3, 0), (0, 0)) = }""")
